58f981fc37041ea2e779af5d04e0d23d7feb8ff7
xxAHTOXAxx <133795951+xxAHTOXAxx@users.noreply.github.com>
2023-11-06 15:03:46 +0400

    settings

---
+++ AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Антон Исаков on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
